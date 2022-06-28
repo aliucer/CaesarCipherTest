ad55c7b18ca05edd5e5bc1b1a1181825d8e803ca
fener <fener@DESKTOP-A6EODBM>
2022-06-28 15:13:44 +0300

    Initial commit

---
+++ PushIt/src/first/App.java
@@ -0,0 +1,10 @@
+package first;
+
+public class App {
+
+	public static void main(String[] args) {
+		CaesarCipher mycaesar = new CaesarCipher();
+		System.out.println(mycaesar.encrypt("xzY", 4));
+	}
+
+}
---
+++ PushIt/src/first/CaesarCipher.java
@@ -0,0 +1,26 @@
+package first;
+
+public class CaesarCipher {
+	
+	private String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	public String encrypt(String plainText, int key) {
+		
+		String cryptedString = "";
+		String plainText2 = plainText.toUpperCase();
+		for(int i=0;i<plainText2.length();i++) {
+			char mychar = plainText2.charAt(i);
+			int place = ALPHABET.indexOf(mychar);
+			place = place + key;
+			place = place % 26;
+			char newChar = ALPHABET.charAt(place);
+			if(Character.isLowerCase(Character.valueOf(plainText.charAt(i)))) {
+				newChar = Character.toLowerCase(newChar);
+			}
+			cryptedString = cryptedString + newChar;
+		}
+		return cryptedString;
+		
+	}
+	
+}
